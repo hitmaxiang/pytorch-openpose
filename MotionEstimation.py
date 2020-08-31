dd8d479be2dda4a80a3d0d5441ed0f40a0e5c90a
mario <maxianghit@163.com>
2020-08-31 21:39:22 +0800

    修改了提取数据时候的错误

--- MotionEstimation.py
+++ MotionEstimation.py
@@ -31,9 +31,17 @@ def ExtractMotionVideo(videopath, name, mode='body'):
         name = './data/%s-bd' % name
     Recpoint = [(350, 100), (700, 400)]
 
+    npyname = '%s.npy' % name
+    if os.path.isfile(npyname):
+        return
+
     index = 0
     while True:
+        # randid = np.random.randint(count)
+        # print('randid: %d' % randid)
+        # video.set(cv2.CAP_PROP_POS_FRAMES, randid)
         ret, frame = video.read()
+        # if ret is False or index > 40:
         if ret is False:
             break
         print('%d/%d' % (index, int(count)), end='\r')
@@ -41,10 +49,11 @@ def ExtractMotionVideo(videopath, name, mode='body'):
         if mode == 'bodyhand':
             PoseMat = EstimationFrame(img, False)
         else:
-            PoseMat = BodyFrame(img)
+            PoseMat = BodyFrame(img, False)
         MotionMat[index, :, :] = PoseMat
         index += 1
     np.save(name, MotionMat)
+    print('')
 
 
 # demonstrate mode
@@ -84,7 +93,7 @@ def EstimationVideo(inputfile):
             break
         '''
         # begin = time.time()
-        EstimationFrame(deepcopy(img))
+        EstimationFrame(deepcopy(img, True))
         # VideoWriter.write(img)
         # print(time.time()-begin)
     videofile.release()
@@ -163,7 +172,7 @@ def BodyFrame(oriImg, display=False):
     BodyMat = np.zeros((18, 3))
     # find the most right person in the screen
     maxindex = None
-    if len(subset) > 1:
+    if len(subset) >= 1:
         leftshoulderX = np.zeros((len(subset),))
         for person in range(len(subset)):
             index = int(subset[person][5])
@@ -176,36 +185,111 @@ def BodyFrame(oriImg, display=False):
             valueindex = int(subset[maxindex][keyindex])
             if valueindex != -1:
                 BodyMat[keyindex, :] = candidate[valueindex][:3]
-
+    
     # draw the body and hand 
     if display is True:
+        # get the data from the save file
+        person = np.zeros((1, 18))
+        position = np.zeros((18, 3))
+        for i in range(18):
+            if sum(BodyMat[i, :]) == 0:
+                person[0, i] = -1
+            else:
+                person[0, i] = i
+            position[i, :3] = BodyMat[i, :]
+
         canvas = deepcopy(oriImg)
+        dataimg = deepcopy(oriImg)
         canvas = util.draw_bodypose(canvas, candidate, subset)
-        plt.imshow(canvas[:, :, [2, 1, 0]])
-        plt.axis('off')
-        plt.show()
+        dataimg = util.draw_bodypose(dataimg, position, person)
+        # plt.imshow(canvas[:, :, [2, 1, 0]])
+        # plt.figure()
+        # plt.imshow(dataimg[:, :, [2, 1, 0]])
+        # plt.axis('off')
+        # plt.show()
+        cv2.imshow('1', canvas)
+        cv2.imshow('2', dataimg)
+        cv2.waitKey(10)
 
     return BodyMat
 
 
+def CheckNpydata(videopath, datapath):
+    video = cv2.VideoCapture(videopath)
+    PoseMat = np.load(datapath)
+    if not video.isOpened():
+        print('the file %s is not exist' % videopath)
+        return
+    count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    if count != PoseMat.shape[0]:
+        print('the count number is not equal')
+    
+    mode = 'bodyhand'
+    if PoseMat.shape[1] == 18:
+        mode = 'body'
+    Recpoint = [(350, 100), (700, 400)]
+
+    index = 0
+    while True:
+        ret, frame = video.read()
+        if ret is False:
+            break
+        print('%d/%d' % (index, int(count)), end='\r')
+        img = frame[Recpoint[0][1]:Recpoint[1][1], Recpoint[0][0]:Recpoint[1][0], :]
+        pose = PoseMat[index, :, :]
+
+        if mode == 'body':
+            subset = np.zeros((1, 20))
+            candidate = np.zeros((20, 4))
+            for i in range(18):
+                if sum(pose[i, :]) == 0:
+                    subset[0, i] = -1
+                else:
+                    subset[0, i] = i
+                candidate[i, :3] = pose[i, :]
+            canvas = util.draw_bodypose(img, candidate, subset)
+            cv2.imshow('img', canvas)
+            key = cv2.waitKey(30)
+            if key & 0xff == ord('q'):
+                break
+        else:
+            pass
+        index += 1
+
+
 if __name__ == "__main__":
     import os
-    # destfolder = '/home/mario/sda/signdata/bbcpose'
-    destfolder = '/home/hit605/public/Upload/mx/bbcpose'
+    import sys
+    args = sys.argv[1:]
+    begin = 0
+    for arg in args:
+        begin = int(arg)
+
+    destfolder = '/home/mario/sda/signdata/bbcpose'
+    # destfolder = '/home/hit605/public/Upload/mx/bbcpose'
     filenames = os.listdir(destfolder)
+
     Code = 2
     if Code == 1:  # random choose one file to estimation
         filename = filenames[np.random.randint(len(filenames))]
         filepath = os.path.join(destfolder, filename)
         EstimationVideo(filepath)
     elif Code == 2:
-        # Pool = multiprocessing.Pool(processes=2)
         filenames.sort()
-        for filename in filenames[80:]:
+        end = min(len(filenames), begin+10)
+        for filename in filenames[begin:end]:
             filepath = os.path.join(destfolder, filename)
             name = filename.split('.')[0]
             print(name)
-            # Pool.apply_async(ExtractMotionVideo, args=(filepath, name))
             ExtractMotionVideo(filepath, name, mode='body')
-        # Pool.close()
-        # Pool.join()
+    elif Code == 3:
+        filenames.sort()
+        num = np.random.randint(len(filenames))
+        for filename in filenames[num:]:
+            filepath = os.path.join(destfolder, filename) 
+            name = filename.split('.')[0]
+            datapath = './data/%s-bd.npy' % name
+            if os.path.isfile(datapath):
+                print(name)
+                CheckNpydata(filepath, datapath)
