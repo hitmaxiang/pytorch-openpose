dd664878f98b6c92a7b0cd9f08b81894dc56ddeb
mario <maxianghit@163.com>
2021-03-10 23:36:15 +0800

    rewrite the dataaugfunction

--- srcmx/DataAugmentation.py
+++ srcmx/DataAugmentation.py
@@ -4,7 +4,7 @@ Version: 2.0
 Autor: mario
 Date: 2021-01-11 16:33:19
 LastEditors: mario
-LastEditTime: 2021-03-09 21:30:46
+LastEditTime: 2021-03-10 23:35:39
 '''
 import os
 import re
@@ -133,7 +133,7 @@ class DataAugmentation():
                         dist = utilmx.SlidingDistance_torch(pattern, segdata)
                         # dist = dist.cpu()
                 else:
-                    dist = utilmx.SlidingDistance(shapeletdata, segdata)
+                    dist = utilmx.SlidingDistance_torch(pattern, segdata)
                 preidx = None
                 # print(min(dist))
                 for idex, dis in enumerate(dist):
@@ -148,8 +148,8 @@ class DataAugmentation():
                         else:
                             AugmentDict[videokey].append((begidx+idex, dis.item()))
                             preidx = idex
-                print('%f seconds-->%s:%d-%d/%d' % (time.time() - begtime, videokey, begidx, endidx, N))
-                begtime = time.time()
+            # print('%f seconds-->%s:%d-%d/%d' % (time.time() - begtime, videokey, begidx, endidx, N))
+            print('the sliding distance of %s with length %d calculate %f seconds' % (videokey, N, time.time()-begtime))
         return AugmentDict
 
     def DataAugmentate(self, word=None, mode=0, scale=0.3, overwrite=False):
@@ -189,8 +189,180 @@ class DataAugmentation():
         h5outfile.close()
 
 
+class FastDataAugmenter():
+    def __init__(self, motionfilepath, shapeletfilepath, scale=0.3, maxseglen=10000):
+        self.motionfilepath = motionfilepath
+        self.shapeletfilepath = shapeletfilepath
+        self.scale = scale
+        self.maxseglen = maxseglen
+        self.featuredatapath = '..data/spbsl/tempfeaturedata.hdf5'
+    
+    def PrepareGlobalData(self, datamode, featuremode, rewrite=False):
+        # 因为每个shapelet 都要和全部的数据进行比对，为了加速计算，对于motiondata的预处理可以先进行
+        # 数据的格式应该是 featuremode/videokey ==> [TxD]
+        motionfile = h5py.File(self.motionfilepath, 'r')
+        videokeys = motionfile['posedata/pose'].keys()
+
+        # 判断哪些数据是需要新添加的
+        writekeys = []
+        with h5py.File(self.featuredatapath, mode='r') as f:
+            for videokey in videokeys:
+                key = '%s-%d/%s' % (datamode, featuremode, videokey)
+                if rewrite is True or key not in f.keys():
+                    writekeys.append(key)
+        
+        if len(writekeys) > 0:
+            print('prepare the motiondata with datamode:%s, featuremode:%d' % (datamode, featuremode))
+
+        for key in writekeys:
+            videokey = key.split('/')[1]
+            posedata = motionfile['posedata/pose/%s' % videokey][:].astype(np.float32)
+            handdata = motionfile['handdata/hand/%s' % videokey][:].astype(np.float32)
+            fram_num = len(posedata)
+            data = np.concatenate((posedata, handdata), axis=1)
+            data = PD.MotionJointFeatures(data, datamode=datamode, featuremode=featuremode)
+            data = np.reshape(data, (data.shape[0], -1))
+
+            utilmx.WriteRecords2File(self.featuredatapath, key, data, (fram_num,), dtype=np.float32)
+        
+        motionfile.close()
+    
+    def SlidingSearching(self, shapelets, datamode, featuremode):
+        featurekey = '%s-%d' % (datamode, featuremode)
+        premotionfile = h5py.File(self.featuredatapath, 'r')
+        
+        for videokey in premotionfile[featurekey].keys():
+            videodata = premotionfile[featurekey][videokey][:]
+            videodata = torch.from_numpy(videodata)
+            if torch.cuda.is_available():
+                videodata.cuda()
+        
+            for shapelet in shapelets:
+                word, shapeletdata, sigma = shapelet
+                levelkey = str(len(shapeletdata))
+                shapeletdata = torch.from_numpy(shapeletdata)
+                if torch.cuda.is_available():
+                    shapeletdata = shapeletdata.cuda()
+                
+        
+        
+
+
+    def GetShapeletpatterns(self, shapeletfilepath, method, scale, words=None):
+        # 从shapeletfile 中提取作为核心的 shaplet, 具有不同的方式，以及精纯度
+        # 一般建议将scale选择较大的， 然后在得到的结果中根据需要进行不同的筛选
+        levelpattern = r'^\d+$'
+        infopattern = r'datamode:(.+)-featuremode:(\d+)$'
+        
+        ShapePatternDict = {}
+
+        shapeletfile = h5py.File(shapeletfilepath, 'r')
+        # 当不给定 words 的时候，默认是所有的 Word
+        if words is None:
+            words = list(shapeletfile.keys())
+
+        for word in words:
+            if word not in shapeletfile.keys():
+                continue
+
+            # 获取该 Word 的 shapelet 的信息
+            ranges = shapeletfile[word]['sampleidxs'][:]
+            videokeys = shapeletfile[word]['videokeys'][:]
+            info = shapeletfile[word]['loginfo'][0]
+            datamode, featuremode = re.findall(infopattern, info)[0]
+            
+            self.PrepareGlobalData(datamode, int(featuremode))
+
+            featurekey = '%s-%s' % (datamode, featuremode)
+            if featurekey not in ShapePatternDict.keys():
+                ShapePatternDict[featurekey] = []
+            
+            # 返回所有找到的shapelet， 各个级别的都要
+            if method == 0:
+                for levelkey in shapeletfile[word].keys():
+                    if re.match(levelpattern, levelkey) is None:
+                        continue
+                    shapelet = shapeletfile[word][levelkey]['shapelet'][:]
+                    dists = np.sort(shapeletfile[word][levelkey]['dists'][:])
+                    sigma = dists[int(len(dists)*scale)]
+                    # shapelet finding 的结果
+                    if len(shapelet) == 1:
+                        index = shapelet[0]
+                        loc = shapeletfile[word][levelkey]['locs'][index]
+                        videokey = videokeys[index]
+                        begidx = ranges[index][0] + loc
+                        
+                        # 从该特征下的预处理motiondata中直接提取片段
+                        with h5py.File(self.featuredatapath, 'r') as f:
+                            shapeletdata = f[featurekey][videokey][begidx:begidx+int(levelkey)]
+                        
+                        ShapePatternDict[featurekey].append((word, shapeletdata, sigma))
+                    
+                    #  the results of shapelet learning 
+                    else:
+                        shapeletdata = shapelet
+                        ShapePatternDict[featurekey].append((word, shapeletdata, sigma))
+            
+            # 对于每个 word 来说, 筛选一个最优的shapelet作为搜索的模板
+            # 这里的最优： score 最高， 然后相对更小的平均距离
+            elif method == 1:
+                # 定义最优的 shapeletinfo
+                bestshaplet = {'score': 0, 'avgdist': float('inf'), 'shapeletinfo': None}
+                for levelkey in shapeletfile[word].keys():
+                    if re.match(levelpattern, levelkey) is None:
+                        continue
+                    shapelet = shapeletfile[word][levelkey]['shapelet'][:]
+                    score = self.shapeletfile[word][levelkey]['score'][0]
+                    dists = np.sort(shapeletfile[word][levelkey]['dists'][:])
+
+                    validnum = int(len(dists) * scale)
+                    sigma = dists[validnum]
+                    avgdist = np.sum(dists[:validnum+1])/validnum
+
+                    if score > bestshaplet['score']:
+                        temp = (levelkey, shapelet, sigma)
+                        bestshaplet['score'] = score
+                        bestshaplet['avgdist'] = avgdist
+                        bestshaplet['shapeletinfo'] = temp
+                    elif score == bestshaplet['score'] and bestshaplet['avgdist'] > avgdist:
+                        temp = (levelkey, shapelet, sigma)
+                        bestshaplet['score'] = score
+                        bestshaplet['avgdist'] = avgdist
+                        bestshaplet['shapeletinfo'] = temp
+                
+                levelkey, shapelet, sigma = bestshaplet['shapeletinfo']
+                # the result of shapelet finding
+                if len(shapelet) == 1:
+                    index = shapelet[0]
+                    loc = shapeletfile[word][levelkey]['locs'][index]
+                    videokey = videokeys[index]
+                    begidx = ranges[index][0] + loc
+                    
+                    # 从该特征下的预处理motiondata中直接提取片段
+                    with h5py.File(self.featuredatapath, 'r') as f:
+                        shapeletdata = f[featurekey][videokey][begidx:begidx+int(levelkey)]
+                    
+                    ShapePatternDict[featurekey].append((word, shapeletdata, sigma))
+                
+                #  the results of shapelet learning 
+                else:
+                    shapeletdata = shapelet
+                    ShapePatternDict[featurekey].append((word, shapeletdata, sigma))
+
+        return ShapePatternDict
+
+
+
+
+
+
+
+
+
+            
+        
 if __name__ == '__main__':
-    shapletfilepath = '../data/spbsl/bk1_shapeletED.hdf5'
+    shapletfilepath = '../data/spbsl/shapeletED.hdf5'
     motiondatapath = '../data/spbsl/motiondata.hdf5'
     outfilepath = '../data/spbsl/augdata.hdf5'
     Augmentation = DataAugmentation(shapletfilepath, outfilepath, motiondatapath, MaxSegLength=100000)
