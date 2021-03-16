f0d88f1e2d4396f66b2dbfffcead48575f16050d
mario <maxianghit@163.com>
2021-03-16 23:12:44 +0800

    rewrite the shapeletmodel function

--- srcmx/shapeletmodel.py
+++ srcmx/shapeletmodel.py
@@ -1,5 +1,8 @@
+from os import dup
 import time
+from numpy.lib.function_base import _copy_dispatcher, copy
 import torch
+from torch._C import dtype, float32
 import utilmx
 
 import numpy as np
@@ -210,33 +213,87 @@ class ShapeletMatrixModel():
 
 # 在这种情况下，手语数据可能要分为左右手的形式
 class ShapeletMatrixModel_LR():
-    def __init__(self):
-        pass
+    def __init__(self, **args):
+        # x-mirror, y-mirror 分别是对奇数和偶数特征维度乘以 -1
+        self.datamodeoptions = ['normal', 'x-mirror', 'y-mirror']
+        self.datamode = self.datamodeoptions[0]
+        # 表示在得到距离之后，二分类的使用方法
+        self.clsoptions = ['fastbipa', 'linear', 'svm']
+        self.clsmethod = self.clsoptions[0]
+        # 表示dis 特征的类型
+        self.disnumoptions = ['global', 'trunk', 'joints']
+        self.disnum = self.disnumoptions
+
+        self.alloptionnames = {'datamode': [self.datamodeoptions, self.datamode], 
+                               'clsoptions': [self.clsoptions, self.clsmethod],
+                               'disnum': [self.disnumoptions, self.disnum]}
+        # 对输入的参数进行解析
+        self.argsparse(args)
     
-    def train(self, X, labels, m_len, dup=1):
+    def argsparse(self, argdict):
+        for key in argdict.keys():
+            if key not in self.alloptionnames.keys():
+                raise ValueError('the input name %s is not correct' % key)
+            
+            value = argdict[key]
+            if isinstance(value, int):
+                if value < 0 or value >= len(self.alloptionnames[key][0]):
+                    self.alloptionnames[key][1] = self.alloptionnames[key][0][value]
+                else:
+                    raise IndexError('the input value %s %d is out of index' % (key, value))
+            
+            elif value in self.alloptionnames[key][0]:
+                self.alloptionnames[key][1] = value
+            
+            else:
+                raise ValueError
+    
+    def preparedata(self, X):
         # X shape batchsize x D x T 或者是 list T x D
         if isinstance(X, list):
+            # 默认list下，数据的类型是 numpy.arrary
             samples = [torch.from_numpy(x).float() for x in X]
             lengths = [len(x) for x in X]
             catsamples = torch.cat(samples, dim=0)
             N, T = len(lengths), max(lengths)
         else:
+            # 如果已经是batch的格式的话，则认为已经是 tensor 的类型了
             N, D, T = X.shape
             catsamples = torch.reshape(X.permute(0, 2, 1), (-1, D))
             lengths = [T] * N
+        # 现在数据catsamples的格式是为 NT x D
+
+        # 下面将根据datamode对数据进行下一步的处理
+        if self.datamode in ['x-mirror', 'y-mirror']:
+            copysamples = torch.clone(catsamples)
+            for i in range(D):
+                if i % 2 == 0 and self.datamode == 'x-mirror':
+                    copysamples[:, i] *= -1
+                elif i % 2 == 1 and self.datamode == 'y-mirror':
+                    copysamples[:, i] *= -1
+
+            catsamples = torch.cat((catsamples, copysamples), dim=0)
+            lengths = lengths * 2
         
-        bestscore = 0
-        bestloss = float('inf')
+        return catsamples, lengths
+    
+    def train(self, X, labels, m_len, **options):
+        # 首先要对模型的参数进行配置
+        self.argsparse(options)
+        catsamples, lengths = self.preparedata(X)
+        N, T = len(lengths), max(lengths)
+        
+        bestscore, miniloss = 0, float('inf')
         cumlength = np.cumsum([0] + lengths)
         shrink = - m_len + 1
-        offset = [x + shrink for x in lengths]
+        validlengths = [x + shrink for x in lengths]
 
-        # 为了避免每次都要重新构建数据库，所以这里提前按照最大的sample构建距离矩阵
-        DISMAT_pre = torch.zeros(T-m_len+1, cumlength[-1]+shrink, dtype=torch.float32)
+        # 为了避免每次都要重新构建数据库，所以这里提前按照最大的sample构建距离矩阵, 存储每个sample与整体的 matrixprofile
+        DISMAT_pre = torch.zeros(T+shrink, cumlength[-1]+shrink, dtype=torch.float32)
 
-        # 同样也会准备存放处理结果数据的
-        MinDis = torch.zeros(N, T-m_len+1, dtype=torch.float32)
-        MinLoc = torch.zeros(N, T-m_len+1, dtype=torch.int16)
+        # 同样也会准备存放处理结果数据的矩阵
+        MinDis = torch.zeros(N, T+shrink, dtype=torch.float32)
+        MinLoc = torch.zeros(N, T+shrink, dtype=torch.int16)
 
         if torch.cuda.is_available():
             catsamples = catsamples.cuda()
@@ -248,7 +305,7 @@ class ShapeletMatrixModel_LR():
         Dis_loc = np.zeros(Dis_sample.shape, dtype=np.int16)
         dis = np.zeros((N,))
         locs = np.zeros((N,), dtype=np.int16)
-        for i in range(N):
+        for i in range(len(labels)):
             if labels[i] == 0:
                 continue
 
@@ -256,19 +313,19 @@ class ShapeletMatrixModel_LR():
             begin, end = cumlength[i:i+2]
             # end = T * (i+1)
             with torch.no_grad():
-                DISMAT_pre[:offset[i]] = utilmx.matrixprofile_torch(
+                DISMAT_pre[:validlengths[i]] = utilmx.matrixprofile_torch(
                     catsamples[begin:end], 
                     catsamples, 
                     m_len, 
-                    DISMAT_pre[:offset[i]])
+                    DISMAT_pre[:validlengths[i]])
 
                 # time_1 = time.time()
 
                 for j in range(N):
                     b_loc = cumlength[j]
                     e_loc = cumlength[j+1] + shrink
-                    tempdis = DISMAT_pre[:offset[i], b_loc:e_loc]
-                    MinDis[j, :offset[i]], MinLoc[j, :offset[i]] = torch.min(tempdis, dim=-1)
+                    tempdis = DISMAT_pre[:validlengths[i], b_loc:e_loc]
+                    MinDis[j, :validlengths[i]], MinLoc[j, :validlengths[i]] = torch.min(tempdis, dim=-1)
 
                 Dis_sample[:] = MinDis.cpu().numpy()
                 Dis_loc[:] = MinLoc.cpu().numpy()
